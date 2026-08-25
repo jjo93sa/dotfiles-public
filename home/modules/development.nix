525ac0b8bf1826e2c92e6c461008afc334277056
James Osborne <jameso@graphcore.ai>
2026-08-25 15:43:54 +0100

    Add common development CLI tools

--- home/modules/development.nix
+++ home/modules/development.nix
@@ -2,12 +2,19 @@
   home.packages = with pkgs; [
     (python3.withPackages (ps: with ps; [pip virtualenv]))
     fd
+    gh
+    glab
     jq
+    just
+    k9s
     kubectl
     mtr
     nixd
     pipenv
     ripgrep
+    shellcheck
+    shfmt
     yamllint
+    yq-go
   ];
 }
